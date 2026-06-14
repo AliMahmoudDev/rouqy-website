8072ca89f268288e863555b7fcdaef96c3dda1a5
AliMahmoudDev <ali@harmens.dev>
2026-06-14 15:07:55 +0000

    chore: add reference screenshot and update build artifacts

--- .next/dev/static/chunks/src_app_page_tsx_47b43e25._.js
+++ .next/dev/static/chunks/src_app_page_tsx_47b43e25._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_7db409dd._.js",
-  "static/chunks/src_46b72884._.js"
+  "static/chunks/src_2c7594c9._.js",
+  "static/chunks/node_modules_7db409dd._.js"
 ],
     source: "dynamic"
 });
--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"19wrIRYoFVVxVNCThKP9Z8zoN9xUdeh7TbEFiLBD+aQ=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Gt59jEf5oWLX+1k9nYWFyspw6/SapnIwUCCt8+aRkJw=\"\n}"
